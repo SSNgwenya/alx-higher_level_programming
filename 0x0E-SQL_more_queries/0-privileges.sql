d2088d0dab5dbd870828da1ff8e6a28f2e179d6c
SSNgwenya <sunnyngwenya@gmail.com>
2024-05-07 05:39:01 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,8 +1,2 @@
-SELECT 
-    grantee,
-    privilege_type
-FROM 
-    information_schema.user_privileges
-WHERE 
-    grantee LIKE 'user\_0d\_1@localhost' OR grantee LIKE 'user\_0d\_2@localhost';
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
