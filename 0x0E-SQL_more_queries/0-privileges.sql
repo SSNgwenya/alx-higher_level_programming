9c785a6f74b53f7f852791b46eb6b9f4118ff3d2
SSNgwenya <sunnyngwenya@gmail.com>
2024-05-07 06:01:51 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,20 @@
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+#!/bin/bash
+
+# MySQL credentials
+MYSQL_USER="root"
+MYSQL_PASSWORD="root"
+MYSQL_HOST="localhost"
+
+# Function to show grants for a user
+show_user_grants() {
+    local user="$1"
+    mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -h"$MYSQL_HOST" -e "SHOW GRANTS FOR '$user'@'$MYSQL_HOST';"
+}
+
+# List privileges for user_0d_1
+echo "Privileges for user_0d_1:"
+show_user_grants "user_0d_1"
+
+# List privileges for user_0d_2
+echo "Privileges for user_0d_2:"
+show_user_grants "user_0d_2"
