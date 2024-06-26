8fef5864afe90ad9220f371cd760da343b613cfa
SSNgwenya <sunnyngwenya@gmail.com>
2024-06-26 15:18:21 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
-//Defining each line
+// Defining each line
 const line1 = 'C is fun';
 const line2 = 'Python is cool';
 const line3 = 'JavaScript is amazing';
 
-//printing each line
+// printing each line
 console.log(line1);
 console.log(line2);
 console.log(line3);
