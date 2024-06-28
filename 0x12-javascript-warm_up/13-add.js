948d3582ebc822688d42b008838e1c47f0e7121c
SSNgwenya <sunnyngwenya@gmail.com>
2024-06-28 08:36:06 -0700

    13-add.js

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-// This script returns the addition of 2 integers
+// This script defines a function that returns the addition of 2 integers
 
 function add(a, b) {
   return a + b;
 }
 
-module.exports = add;
+module.exports = { add };
 
