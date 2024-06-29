c1f2ad7b7adccc4e5a58075ae02e59a93e9a0c0b
SSNgwenya <sunnyngwenya@gmail.com>
2024-06-29 13:11:51 -0700

    102-add_me_maybe.js

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Defining a function that increase a number and calls another function
+
+function addMeMaybe(number, theFunction) {
+  number++;
+  theFunction(number);
+}
+module.exports = { addMeMaybe };
