78ac46d5a18b383e088dc76505afc95344118a20
SSNgwenya <sunnyngwenya@gmail.com>
2024-07-10 04:48:02 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 // This script imports an array and computes a new array
 
-const { list } = require('./100-data');
-const transformedList = list.map((value, index) => value * index);
+const { list } = require('./100-data.js');
+
+const newList = list.map((value, index) => value * index);
 console.log(list);
-console.log(transformedList);
+console.log(newList);
