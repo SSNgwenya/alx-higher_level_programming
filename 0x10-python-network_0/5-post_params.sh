c4e49fd223e1dadb672414c0ff2f63d38316f10a
SSNgwenya <sunnyngwenya@gmail.com>
2024-07-19 07:15:17 -0700

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in URL, sends POST request to passed URL, & displays body of response, variable email must be sent with value hr@holbertonschool.com, variable subject must be sent with value I will always be here for PLD
- curl -s "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# takes in URL, sends POST request to passed URL, & displays body of response, variable email must be sent with value test@gmail.com, variable subject must be sent with value I will always be here for PLD
+ curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
