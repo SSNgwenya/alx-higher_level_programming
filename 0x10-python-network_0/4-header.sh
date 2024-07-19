a4ad0cce64c51aaae3f84d53f1bf6d0593e360f4
SSNgwenya <sunnyngwenya@gmail.com>
2024-07-19 07:12:09 -0700

    4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in URL as argument, sends GET request to URL, & displays body of response, A header variable X-HolbertonSchool-User-Id must be sent with value 98
-curl -s "$1" -H "X-HolbertonSchool-User-Id: 98"
+# takes in URL as argument, sends GET request to URL, & displays body of response, A header variable X-School-User-Id must be sent with value 98
+curl -s "$1" -H "X-School-User-Id: 98"
