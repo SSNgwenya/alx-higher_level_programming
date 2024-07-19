0b3de5775aa597549ad7a4e93591496d08e0e58b
SSNgwenya <sunnyngwenya@gmail.com>
2024-07-19 07:53:46 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 """
-fetches https://alx-intranet.hbtn.io/status
+Script that fetches https://alx-intranet.hbtn.io/status
 """
-
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+def main():
+    """
+    Function to print a response of a specific URL
+    """
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
-
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode('utf-8')))
-
+if __name__ == "__main__":
+    main()
