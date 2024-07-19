d18432d3856c1ec8c89ca6f153d855efad3d8755
SSNgwenya <sunnyngwenya@gmail.com>
2024-07-19 08:04:28 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """
-Module using request that fetches https://intranet.hbtn.io/status
+Module using requests that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
-
 def main():
     """
-    Function that fetches https://intranet.hbtn.io/status
+    Function that fetches https://alx-intranet.hbtn.io/status
     """
-    url = 'https://intranet.hbtn.io/status'
+    url = 'https://alx-intranet.hbtn.io/status'
     r = requests.get(url)
+    body = r.text
+
     print('Body response:')
-    print('\t- type: {}'.format(type(r.text)))
-    print('\t- content: {}'.format(r.text))
+    print('\t- type: {}'.format(type(body)))
+    print('\t- content: {}'.format(body))
 
 if __name__ == "__main__":
     main()
+
